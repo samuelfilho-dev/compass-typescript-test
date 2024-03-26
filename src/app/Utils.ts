9b42bd6c83d6d7d7e2ab8c91ca0a4f64805413f1
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-26 00:40:09 -0300

    feat: add new test

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string) {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowerCase: string;
+  uppercase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+export function getStingInfo(arg: String): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    uppercase: arg.toUpperCase(),
+    characters: Array.from(arg),
+    length: arg.length,
+    extraInfo: {},
+  };
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,11 +1,35 @@
-import { toUpperCase } from "../app/Utils";
+import { getStingInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
+  it("Should return uppercase valid string", () => {
+    // ** Tiple AAA Structure **
+    // Arrange
+    const sut = toUpperCase;
+    const expected = "ABC";
 
-  it("Should return uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+    //Act
+    const actual = sut("abc");
+
+    //Assert
+    expect(actual).toBe(expected);
   });
 
-  
+  it.only('should return info for valid string', () => {
+    const acutal = getStingInfo('My-String');
+
+    expect(acutal.lowerCase).toBe('my-string');
+    expect(acutal.extraInfo).toEqual({});
+
+    expect(acutal.characters).toHaveLength(9);
+    expect(acutal.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
+    expect(acutal.characters).toContain<string>('M');
+    expect(acutal.characters).toEqual(
+        expect.arrayContaining(['S', 't', 'r','i', 'n', 'g','M', 'y', '-'])
+    );
+
+    expect(acutal.extraInfo).not.toBe(undefined);
+    expect(acutal.extraInfo).not.toBeUndefined();
+    expect(acutal.extraInfo).toBeDefined();
+    expect(acutal.extraInfo).toBeTruthy();
+  });
 });
