26648f02626bd4590c7746fa70bcdc8978dded9c
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-01 10:54:27 -0300

    feat: add admin password

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -46,11 +46,24 @@ describe('PasswordChecker test suite', () => {
         expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE); 
     });
 
-    it('Complex ', () => {
+    it('Complex password is valid', () => {
         const actual = sut.checkPassword('1234abcD');
         
         expect(actual.reasons).toHaveLength(0);
         expect(actual.valid).toBe(true);
+    });
+
+    it('Admin password with no number is invalid', () => {
+        const actual = sut.checkAdminPassword('abcdABCD');
+
+        expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
+        expect(actual.valid).toBe(false);
+    });
+
+    it('Admin password with number is valid', () => {
+        const actual = sut.checkAdminPassword('abcdABCD7');
+
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
     });
 
 });
