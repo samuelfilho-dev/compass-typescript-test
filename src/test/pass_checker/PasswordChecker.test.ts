b858a556b3cd30e7a1f0549f61986282a28d1840
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-31 21:36:14 -0300

    feat: add new password checker test cases

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -7,8 +7,40 @@ describe('PasswordChecker test suite', () => {
         sut = new PasswordChecker();
     });
 
-    it('Should do nothing for the moment', () => {
-        sut.checkPassword();
+    it('Password with less than 8 characters is invalid', () => {
+        const actual = sut.checkPassword('1234567');
+
+        expect(actual).toBe(false);
+    });
+
+    it('Password with more than 8 characters is ok', () => {
+        const actual = sut.checkPassword('12345678Aa');
+        
+        expect(actual).toBe(true);
+    });
+
+    it('Password with no upper case letter is invalid ', () => {
+        const actual = sut.checkPassword('1234abcd');
+        
+        expect(actual).toBe(false);
+    });
+
+    it('Password with upper case letter is valid ', () => {
+        const actual = sut.checkPassword('1234abcdA');
+        
+        expect(actual).toBe(true);
+    });
+
+    it('Password with no lower case letter is invalid ', () => {
+        const actual = sut.checkPassword('1234ABCD');
+        
+        expect(actual).toBe(false);
+    });
+
+    it('Password with lower case letter is valid ', () => {
+        const actual = sut.checkPassword('1234ABCDa');
+        
+        expect(actual).toBe(true);
     });
 
 });
