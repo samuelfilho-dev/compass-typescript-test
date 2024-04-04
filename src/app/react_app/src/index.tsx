7a3b79195bc6142a65df5c5b19752ce29e6c8618
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-04 01:08:43 -0300

    feat: add react projetc

---
+++ src/app/react_app/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App2 from './App2';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const loginService = new LoginService();
+
+const setToken = (token: string) => {
+  console.log(`received token ${token}`);
+};
+
+root.render(
+    <LoginComponent loginService={loginService} setToken={setToken} />
+);
+
